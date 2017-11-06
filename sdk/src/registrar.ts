af466aa2ad9030771b4a13d204af0af1f9f28f38
Lynn Neir <lynn@symphony.com>
2017-11-05 17:18:57 -0800

    add methods to assist in testing

--- sdk/src/registrar.ts
+++ sdk/src/registrar.ts
@@ -8,6 +8,9 @@ export type constructor<T> = new(...args: any[]) => T;
 export class Registrar {
     private map: Map<InterfaceIdentifier,any>;
 
+    private clonedMap: Map<InterfaceIdentifier,any>;
+    private waitingForRestore: boolean;
+
     constructor() {
         this.map = new Map();
     }
@@ -16,10 +19,21 @@ export class Registrar {
         this.map.set(identifier, constructor)
     }
 
+    /**
+     * Removes a previously bound interface.  Primarily intended for testing
+     * and used in conjuction with snapshot/restore.
+     * @param  {InterfaceIdentifier} identifier Identifier to be removed (if exists)
+     */
+    unbind<T>(identifier:InterfaceIdentifier) {
+        if (this.map.has(identifier)) {
+            this.map.delete(identifier);
+        }
+    }
+
     get<T>(identifier:InterfaceIdentifier, ...args) {
         let C = this.map.get(identifier);
         if (!C) {
-                throw new Error('No registration found for: ' + identifier.toString());
+            throw new Error('No registration found for: ' + identifier.toString());
         }
         let c = new C(args);
         return (c as T);
@@ -52,6 +66,32 @@ export class Registrar {
         });
 
         return Promise.all(promises);
+    }
+
+    /**
+     * snapshot and restore are intended to be used for testing purposes...
+     * registrar.snapshot();
+     * registar.unbind<T1>(T1Symbol);
+     * reigstar.bind<T1>(T1Symbol, mockT1);
+     * ...do testing...
+     * registar.restore();
+     */
+    snapshot() {
+        if (this.waitingForRestore) {
+            throw new Error('Must call restore before calling snapshot again.');
+        }
+        this.waitingForRestore = true;
+        this.clonedMap = new Map(this.map);
+    }
+
+    restore() {
+        if (this.clonedMap) {
+            this.map = this.clonedMap;
+            this.clonedMap = null;
+            this.waitingForRestore = false;
+        } else {
+            throw new Error('No cloned map exists.')
+        }
     }
 }
 
