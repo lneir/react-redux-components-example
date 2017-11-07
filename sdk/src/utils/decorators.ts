d1a9f6c198f8b1d285fbaf791f709d70d052701f
Lynn Neir <lynn@symphony.com>
2017-11-06 20:49:23 -0800

    clean up decorators.ts

--- sdk/src/utils/decorators.ts
+++ sdk/src/utils/decorators.ts
@@ -1,23 +1,13 @@
 import proxyGetter from './proxy';
 import registrar from '../registrar';
 
-// function getDecorators() {
-
-    // Decorator used to bind action creators and inject them into a class property
-   let injectInterface = (interfaceIdentifier: symbol) => {
-       return function(proto: any, key: string): void {
-        //    let dispatch = store.dispatch;
-        //    let actionCreators = kernel.get(actionCreatorsIdentifier);
-        //    let boundActionCreators = Redux.bindActionCreators(actionCreators, dispatch);
-           let resolve = () => {
-               return registrar.get(interfaceIdentifier); };
-           proxyGetter(proto, key, resolve);
-       };
+let injectInterface = (interfaceIdentifier: symbol) => {
+    return function(proto: any, key: string): void {
+       let resolve = () => { return registrar.get(interfaceIdentifier); };
+       proxyGetter(proto, key, resolve);
    };
+};
 
-   export {
-       injectInterface
-   }
-// }
-//
-// export default getDecorators;
+export {
+   injectInterface
+}
